c7dcde2cd4b5bacb268799a7d9a91bd697b78436
Dennis Hahn <dh7qc@mst.edu>
2016-09-12 16:39:49 -0500

    Fixed flake8 complaints in test_main

--- test_main.py
+++ test_main.py
@@ -12,9 +12,9 @@ def test_press_button():
 
     Tests press_button function.
     """
-    # Needs to be True when input is divisible by 17. 
+    # Needs to be True when input is divisible by 17.
 
-    # The following should be True. 
+    # The following should be True.
     valid = [-34, -17, 0, 17, 34, 51, 68, 85]
     # The following should be False (numbers incremented by 1).
     invalid = [num + 1 for num in valid]
@@ -35,8 +35,8 @@ def test_which_to_press():
     """
     # Format: which_to_press(history, displayed)
     # History Format: (pressed, displayed) tuple.
-    
-    # If display shows 1, always return 4. 
+
+    # If display shows 1, always return 4.
     '''
     x = [(1, 4), (25, 100)]
     assert(which_to_press(None, 1) == 4)
@@ -44,12 +44,13 @@ def test_which_to_press():
     # If display shows 2, return value pressed in previous round.
     assert(which_to_press(x, 2) == 100)
 
-    # If display shows 3, return value displayed in previous round. 
+    # If display shows 3, return value displayed in previous round.
     assert(which_to_press(x, 3) == 25)
 
     # If display shows 4...
     '''
     pytest.xfail("...")
+
 
 def test_dial_to():
     """Test that we'll know how to respond to the Code Layer
@@ -58,24 +59,24 @@ def test_dial_to():
     """
 
     # dial_to(state, code)
-    # Set up a state with a test serial number. 
+    # Set up a state with a test serial number.
     state = {
         'suspicion level': 0,
         'indicators': {},
     }
     state['serial number'] = 'JQXX7e3652'
 
-    # Test code 
+    # Test code
     code = 'circuit'
-    
+
     # dial_to should return a 'c'
     assert(dial_to(state, code) == 'c')
 
-    # Test another example, should return 'a'. 
+    # Test another example, should return 'a'.
     state['serial number'] = 'XX7e3652'
     code = 'elephant'
     assert(dial_to(state, code) == 'a')
-    
+
 
 def test_should_flip():
     """Test that we'll know how to respond to the Switches Layer
@@ -84,15 +85,15 @@ def test_should_flip():
     """
 
     # Function: should_flip(bag_state, has_red, has_blue, has_green)
-    
-    # Test state and serial number: 
+
+    # Test state and serial number:
     test_state = {
         'suspicion level': 0,
         'indicators': {},
     }
     test_state['serial number'] = 'JQXX7e3652'
     test_state['indicators']['check engine'] = False
-    test_state['indicators']['everything ok'] = True 
+    test_state['indicators']['everything ok'] = True
 
     # Label D, All off, return False.
     assert(should_flip(test_state, False, False, False) is False)
@@ -111,4 +112,3 @@ def test_should_flip():
 
     # Label Y, only blue light on. False (No Y in serial).
     assert(should_flip(test_state, False, True, False) is False)
-
